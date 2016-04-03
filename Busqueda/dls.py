2cc98ae382f8642e082e0cad16a7804f0e027f8c
Guilermo Perez Villarreal <409guillermos@gmail.com>
2016-04-02 19:48:36 -0600

    Added files via upload

---
+++ Busqueda/dls.py
@@ -0,0 +1,20 @@
+class DLS:
+
+    @staticmethod    
+    def search(start,stop,depth):
+        agenda=deque()
+        explored=set()
+        if(stop(start)):
+            return trajectory(start)
+        elif(depth==0):
+            return None
+        agenda.append(start)
+        while(agenda):
+            nodo=agenda.pop()
+            explored.add(nodo)
+            for child in nodo.expand():
+                if(stop(child)):
+                    return trajectory(child)
+                elif(not child in explored and child.depth<depth-1):
+                    agenda.append(child)
+        return None
---
+++ Busqueda/id.py
@@ -0,0 +1,24 @@
+class ID:
+
+    @staticmethod    
+    def search(start,stop):
+        agenda=deque()
+        explored=set()
+        if(stop(start)):
+            return trajectory(start)
+        agenda.append(start)
+        depth=1
+        while(True):
+            while(agenda):
+                nodo=agenda.pop()
+                explored.add(nodo)
+                for child in nodo.expand():
+                    if(stop(child)):
+                        return trajectory(child)
+                    elif(not child in explored and child.depth<depth-1):
+                        agenda.append(child)
+            depth+=1
+            agenda=deque()
+            explored=set()
+            agenda.append(start)
+
