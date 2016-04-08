edf4a826f5b764c48a09d4e72c4330d7ff3d868e
Guilermo Perez Villarreal <409guillermos@gmail.com>
2016-04-07 22:53:39 -0500

    Update dls.py

--- Busqueda/dls.py
+++ Busqueda/dls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Artificial Intellence 1766
 Engineering Faculty
@@ -11,7 +12,7 @@ Algoritmo de Busqueda DLS, implementado para un puzzle de 15 piezas
 Ejecucion:
     p = Puzzle()
     p.shuffle(10)
-    solucion = Aestrella.search(p,lambda n: n == Puzzle(),maxDepth)
+    solucion = DLS.search(p,lambda n: n == Puzzle(),maxDepth)
     print (solucion)
 
 """
@@ -24,17 +25,20 @@ class DLS:
     def search(start,stop,depth):
         agenda=deque()
         explored=set()
+        memoria=1
         if(stop(start)):
-            return trajectory(start)
+            return (trajectory(start),memoria)
         elif(depth==0):
-            return None
+            return (None,0)
         agenda.append(start)
         while(agenda):
             nodo=agenda.pop()
             explored.add(nodo)
             for child in nodo.expand():
                 if(stop(child)):
-                    return trajectory(child)
+                    return (trajectory(child),memoria)
                 elif(not child in explored and child.depth<depth-1):
                     agenda.append(child)
-        return None
+                    if(len(agenda)>memoria):
+                        memoria=len(agenda)
+        return (None,0)
